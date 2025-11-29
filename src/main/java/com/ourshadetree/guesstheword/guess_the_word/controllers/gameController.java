38e7ba0f8ea22947d8cee782a119e1c33af1c592
ourshadetree <ostcreate@gmail.com>
2025-11-28 21:51:59 -0500

    added input box
    app can now capture a character input from the user

--- src/main/java/com/ourshadetree/guesstheword/guess_the_word/controllers/gameController.java
+++ src/main/java/com/ourshadetree/guesstheword/guess_the_word/controllers/gameController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ourshadetree.guesstheword.guess_the_word.service.GameService;
 
@@ -15,7 +16,10 @@ public class gameController {
     GameService gameService;
     
     @GetMapping("game-home")
-    public String showGameHomePage(Model model) {
+    public String showGameHomePage(@RequestParam(value = "guessedChar", required = false) String guessedChar, Model model) {
+
+        System.out.println("captured guessed word is " + guessedChar);
+
         String randomWord = gameService.toString();
         model.addAttribute("wordToDisplay", randomWord);
         return "game-home-page";
